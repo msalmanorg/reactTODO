48bbfb5c2d95a3aada961f0b50544a2b3471c85e
Salman <96868027+msalmanorg@users.noreply.github.com>
2022-06-15 22:27:43 -0700

    Update todo.js

--- src/Components/todo.js
+++ src/Components/todo.js
@@ -6,10 +6,10 @@ const Todo = () => {
     // Get data from the Local Storage 
     const LocalData = () => {
         const data = localStorage.getItem("myTodo");
-        if(data){
+        if (data) {
             return JSON.parse(data);
         }
-        else{
+        else {
             return [];
         }
     }
@@ -19,8 +19,8 @@ const Todo = () => {
     const [inputData, setInputData] = useState('');
     const [items, setItems] = useState(LocalData);
 
-    const [button , setButton] = useState(false);
-    const [number , setNumber] = useState('');
+    const [button, setButton] = useState(false);
+    const [number, setNumber] = useState('');
 
 
     // Add New Items 
@@ -50,47 +50,47 @@ const Todo = () => {
 
 
     // Calling a Function on pressing Enter key
-    const useOnKey=  (callback , targetKey) =>  {
+    const useOnKey = (callback, targetKey) => {
         useEffect(() => {
             const Keypresser = (e) => {
-                if(e.key === targetKey) {
+                if (e.key === targetKey) {
                     callback();
                 }
             }
-            window.addEventListener("keydown" , Keypresser);
+            window.addEventListener("keydown", Keypresser);
             return () => {
-                window.removeEventListener("keydown" , Keypresser)
+                window.removeEventListener("keydown", Keypresser)
             }
-        } , [callback , targetKey])
+        }, [callback, targetKey])
     }
 
 
-    
+
 
 
 
     // Edit items
     const EditItem = (index) => {
-        setButton(true)
+        setButton(true);
         const data = items.find((curElem) => {
             return curElem.id === index;
         })
-        
+
         setNumber(data.id);
-        setInputData(data.name);  
-          
+        setInputData(data.name);
+        // document.getElementById("input").click();
     }
 
- 
+
     // Update editAble items 
     const UpdateItems = () => {
         const data = items.find((curElem) => {
             return curElem.id === number
         })
-        if(inputData === '') {
+        if (inputData === '') {
             alert("Please enter Something in the input field !");
         }
-        else{
+        else {
             data.name = inputData;
             setInputData('')
         }
@@ -100,39 +100,39 @@ const Todo = () => {
 
     // Set data to Local Storage
     useEffect(() => {
-        localStorage.setItem("myTodo" , JSON.stringify(items));
-        document.title  = `Note Pad (${items.length})`
-    } , [items , setItems ,  UpdateItems])
+        localStorage.setItem("myTodo", JSON.stringify(items));
+        document.title = `Note Pad (${items.length})`
+    }, [items, setItems, UpdateItems])
 
 
 
     // Pressing the  Enter key to perform the functions 
-    useOnKey(button ? UpdateItems :  AddItems , 'Enter'  )
+    useOnKey((button ? UpdateItems : AddItems), 'Enter')
 
 
 
     return (
-        <> 
+        <>
             <div className="main-div" id='main'>
                 <div className="child-div">
- 
+
                     {/* figure Section */}
 
                     <figure>
-                        <img src="notePad.png" alt='d' />
+                        <img src="https://msalmanorg.github.io/reactTODO/notePad.png" alt='d' />
                         <figcaption>And Your List Here 🤟</figcaption>
                     </figure>
 
                     <div className="addItems">
-                        <input  type="text" autoFocus={true} id='input' placeholder='✍ Add Item' className='form-control'
+                        <input type="text" autoFocus={true} id='input' placeholder='✍ Add Item' className='form-control'
                             onChange={(e) => setInputData(e.target.value)}
                             value={inputData}
                         />
                         {
-                            button ? 
-                            (<i  onClick={() => UpdateItems()} className="fa fa-edit add-btn" ></i> ) 
-                            :
-                            ( <i onClick={() => AddItems()} className="fa fa-plus add-btn" ></i>)
+                            button ?
+                                (<i onClick={() => UpdateItems()} className="fa fa-edit add-btn" ></i>)
+                                :
+                                (<i onClick={() => AddItems()} className="fa fa-plus add-btn" ></i>)
                         }
                     </div>
 
@@ -142,16 +142,18 @@ const Todo = () => {
                     <div className="showItems">
 
                         {
-                            items.map((curElem) => {
+                            items.map((curElem, index) => {
                                 return (
-                                    <div className="eachItem" >
+                                    <div className="eachItem" key={index} >
                                         <h3>{curElem.name}</h3>
                                         <div className="todo-btn " id='todo-btn'>
-                                            <i className="far fa-edit add-btn" onClick={() => EditItem(curElem.id)} ></i>
+                                            <label htmlFor="input">
+                                                <i className="far fa-edit add-btn" onClick={() => EditItem(curElem.id)} ></i>
+                                            </label>
                                             <i className="far fa-trash-alt add-btn" onClick={() => DeleteItem(curElem.id)} ></i>
                                         </div>
                                     </div>
-                                ) 
+                                )
                             })
                         }
 
